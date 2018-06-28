f290b730b924cd0386a9c78ac0f7e515edadeaab
Sean Oughton <seanoughton@gmail.com>
2018-06-28 17:02:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,4 +28,8 @@ class BoardMember {
 
 class Ceo extends BoardMember{
 
+  hireEmployee() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+  };
+
 };
